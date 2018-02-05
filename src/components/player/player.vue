960a645a10add13838479cb43cc1c3fd6e451694
wangjingli <wangjingli@zillionfortune.com>
2018-02-05 18:01:32 +0800

    progressBar

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -5,20 +5,7 @@
         </div>
 
         <div class="mod-song-control grid">
-            <div class="progress-wrapper">
-                <span class="ticker cur">00:17</span>
-                <div class="progress">
-                    <div class="bar noUi-target noUi-ltr noUi-horizontal noUi-connect" @click="programBarClick">
-                        <div class="noUi-base">
-                            <div class="noUi-origin noUi-background" :style="{left: percent+'%'}">
-                                <div class="noUi-handle noUi-handle-lower"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <span class="duration">04:50</span>
-            </div>
-
+            <progressBar :percent="percent" />
             <ul class="action row" style="list-style: none;display: flex; align-items: center;justify-content: space-between">
                 <li class="iconfont icon-loop col-3 iconmode" style="text-align: left;"></li>
                 <li class="col-6 iconcenter">
@@ -34,7 +21,11 @@
     </div>
 </template>
 <script>
+import progressBar from '../../base/progressBar/progressBar'
 export default {
+  components:{
+    progressBar
+  },
   data() {
     return {
       playing: false,
@@ -59,7 +50,7 @@ export default {
     updateTime(e) {
       this.currentTime = e.target.currentTime;
     },
-    programBarClick(e) {
+    progressClick(e) {
       const programPrecent = (e.clientX - (375 - 269) / 2) / 269;
       const currentTime = programPrecent * 241;
       this.$refs.audio.currentTime = currentTime;
@@ -240,85 +231,7 @@ export default {
   margin-right: -9px;
 }
 
-.mod-song-control .progress-wrapper {
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: flex;
-  -webkit-box-align: center;
-  -webkit-align-items: center;
-  align-items: center;
-  -webkit-box-pack: justify;
-  -webkit-justify-content: space-between;
-  justify-content: space-between;
-}
 
-.mod-song-control .progress-wrapper .duration,
-.mod-song-control .progress-wrapper .ticker {
-  display: block;
-  color: #ccc;
-  font-size: 12px;
-  line-height: 2.4;
-}
-
-.mod-song-control .progress-wrapper .duration {
-  text-align: right;
-}
-
-.mod-song-control .progress-wrapper .progress {
-  -webkit-box-flex: 1;
-  -webkit-flex: 1;
-  flex: 1;
-  padding: 0 6px;
-}
-
-.mod-song-control .progress-wrapper .progress .bar {
-  width: 100%;
-  height: 1px;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  background: #f8f8f8;
-}
-
-.mod-song-control .progress-wrapper .progress .bar .noUi-base {
-  position: relative;
-  width: 100%;
-  height: 1px;
-  display: block;
-  background-color: #999;
-}
-
-.mod-song-control .progress-wrapper .progress .bar .noUi-origin {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  background: #e5e5e5;
-}
-
-.mod-song-control .progress-wrapper .progress .bar .noUi-handle {
-  position: absolute;
-  z-index: 99;
-  width: 10px;
-  height: 10px;
-  top: -4px;
-  left: -3px;
-  background-color: #333;
-  -webkit-border-radius: 50%;
-  border-radius: 50%;
-  -webkit-box-shadow: 0 0 4px rgba(255, 255, 255, 0.5);
-  box-shadow: 0 0 4px rgba(255, 255, 255, 0.5);
-}
-
-.mod-song-control .progress-wrapper .progress .bar .noUi-handle::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 30px;
-  height: 30px;
-  margin-top: -10px;
-  margin-left: -10px;
-}
 
 .mod-song-control .action {
   padding-top: 12px;
